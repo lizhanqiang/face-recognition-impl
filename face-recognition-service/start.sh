9299c56e87136843875fb74b22e053b05009491e
lizhanq <282650555@qq.com>
2020-07-26 20:22:49 +0800

    change jenkinsfile

--- face-recognition-service/start.sh
+++ face-recognition-service/start.sh
@@ -3,3 +3,5 @@
 ps aux | grep face-recognition-service | grep -v grep | awk '{print $2}' | xargs kill -9
 
 nohup java -jar target/face-recognition-service-1.0-SNAPSHOT.jar >service.log  2>&1 &
+
+exit 0
