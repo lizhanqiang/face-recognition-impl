d9e789b99518ff08e908458478b4eac5308b0f57
lizhanq <282650555@qq.com>
2020-07-26 20:37:36 +0800

    change jenkinsfile

--- face-recognition-service/start.sh
+++ face-recognition-service/start.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/sh
 
-export BUILD_ID=dontKillMe
-
 ps aux | grep face-recognition-service | grep -v grep | awk '{print $2}' | xargs kill -9
 
-nohup java -jar target/face-recognition-service-1.0-SNAPSHOT.jar >service.log  2>&1 &
+JENKINS_NODE_COOKIE=dontKillMe nohup java -jar target/face-recognition-service-1.0-SNAPSHOT.jar >service.log  2>&1 &
